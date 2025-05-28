1fc884b5128290fffe199aeefbd723832ac0cd75
Krishnaraj286g <krishnaraj286@gmail.com>
2025-05-28 10:08:07 +0530

    Reaveal added

--- script.js
+++ script.js
@@ -121,3 +121,22 @@ seemorebtn.addEventListener("click",() => {
     });
     seemorebtn.style.display = "none";
 })
+
+
+// reaveal
+
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+reveals.forEach(reveal => {
+  observer.observe(reveal);
+});
